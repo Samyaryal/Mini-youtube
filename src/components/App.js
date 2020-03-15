6c189765b82e1b48eca23995c820fd562eecc04f
Samikshya <samikshyaaryal5@gmail.com>
2020-03-15 23:06:22 +0200

    nested callbacks and styling videodetail

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,28 @@ import React from 'react'
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component{
-    state= {videos: []};
+    state= {videos: [], selectedVideo: null};
     onTermSubmit = async term => {
         const response = await youtube.get('/search', { //accessing search end point
             params:{
-                q: term
+                q: term // this is from youtube so have to use always "q"
             }
         }); 
         
         this.setState({videos: response.data.items});
+    };
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video}); //updates the state at App class
     }
     render(){
         return ( //
             <div className="ui container">
                 <SearchBar onFormSubmit = {this.onTermSubmit} />
-                <VideoList videos={this.state.videos} /> 
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList onVideoSelect ={this.onVideoSelect} videos={this.state.videos} /> 
             </div>
         );
     }
